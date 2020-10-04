e11a8380c5a5509768f55b49e1efefa9ecf8dcbd
Ivan Brovenko <ivanbrovenko@MacBook-Pro-Ivan.local>
2020-10-04 20:02:52 +0300

    add tab bar

--- src/navigator/HomeStack.js
+++ src/navigator/HomeStack.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import ContentScreen from '../screens/ContentScreen'
 const Stack = createStackNavigator();
-
+const Tab = createBottomTabNavigator()
 const HomeStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Account" component={HomeScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} options={{header: () => true}}/>
+      <Tab.Screen name="Content" component={ContentScreen} />
+    </Tab.Navigator>
   );
 };
 
