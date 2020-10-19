426ad1529a0b114fe4db37b3380aad4f75eeb5da
Ivan Brovenko <ivanbrovenko@MacBook-Pro-Ivan.local>
2020-10-19 19:00:37 +0300

    post form

---
+++ src/components/PostForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const PostForm = () => {
+    return(
+        <View>
+            <Text>Post form</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+
+})
+
+export default PostForm
