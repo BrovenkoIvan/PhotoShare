97dc39e112af805f6670547c3a8a1770dc88f3d0
Ivan Brovenko <ivanbrovenko@MacBook-Pro-Ivan.local>
2020-10-19 19:00:00 +0300

    navigator

--- src/navigator/HomeStack.js
+++ src/navigator/HomeStack.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createStackNavigator, HeaderTitle} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
-import ContentScreen from '../screens/ContentScreen'
+import ContentScreen from '../screens/ContentScreen';
+import PostFormScreen from '../screens/PostFormScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
+
+const ContentStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Content" component={ContentScreen} />
+      <Stack.Screen
+        name="Post Form"
+        component={PostFormScreen}
+        options={{header: () => false}}
+      />
+    </Stack.Navigator>
+  );
+};
+const Home = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
 const HomeStack = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} options={{header: () => true}}/>
-      <Tab.Screen name="Content" component={ContentScreen} />
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color}) => {
+          let iconName;
+          if (route.name === 'Home')
+            iconName = focused ? 'home' : 'home-outline';
+          else if (route.name === 'Content')
+            iconName = focused ? 'list-circle' : 'list-circle-outline';
+          return <Ionicons name={iconName} size={38} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'rgb(0, 205, 251)',
+        inactiveTintColor: 'rgb(130, 80, 90)',
+        showLabel: false,
+      }}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Content" component={ContentStack} />
     </Tab.Navigator>
   );
 };
--- src/navigator/Routes.js
+++ src/navigator/Routes.js
@@ -1,34 +1,43 @@
 import React, {useState, useEffect, useContext} from 'react';
 import HomeStack from './HomeStack';
 import LoginStack from './LoginStack';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
 import {Context} from '../context/AuthContext';
 import auth from '@react-native-firebase/auth';
-import Loading from '../components/Loading'
+import Loading from '../components/Loading';
+import {AppearanceProvider, useColorScheme, Appearance} from 'react-native';
+
+
 const Routes = () => {
-  const { state } = useContext(Context);
-    const [user, setUser] = useState(null)
+  const scheme = useColorScheme();
+  const {state} = useContext(Context);
   const [loading, setLoading] = useState(true);
   const [initializing, setInitializing] = useState(true);
-  console.log(state)
 
   const onAuthStateChanged = (user) => {
-    setUser(user);
-    if (initializing) setInitializing(false);
-    setLoading(false);
+    state.user = user;
+    
+    if (initializing) {
+      setInitializing(false);
+      setLoading(false);
+    }
   };
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
-  });
-  
+  }, []);
   if (loading) {
     return <Loading />;
   }
   return (
-    <NavigationContainer>
-      {user ? <HomeStack /> : <LoginStack />}
+    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
+      {state.user ? <HomeStack /> : <LoginStack />}
     </NavigationContainer>
+
   );
 };
 
